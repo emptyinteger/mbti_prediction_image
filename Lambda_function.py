1f6bed7e5ebad3b794dc58be6a0af8526efe4c05
emptyinteger <92441821+emptyinteger@users.noreply.github.com>
2022-10-31 03:17:16 +0900

    Update Lambda_function.py

--- Lambda_function.py
+++ Lambda_function.py
@@ -15,36 +15,22 @@ import tensorflow_addons as tfa
 import io
 import boto3
 
-s3 = boto3.client('s3')
+def download_dir(client, resource, dist, local='/tmp', bucket='your_bucket'):
+    paginator = client.get_paginator('list_objects')
+    for result in paginator.paginate(Bucket=bucket, Delimiter='/', Prefix=dist):
+        if result.get('CommonPrefixes') is not None:
+            for subdir in result.get('CommonPrefixes'):
+                download_dir(client, resource, subdir.get('Prefix'), local, bucket)
+        for file in result.get('Contents', []):
+            dest_pathname = os.path.join(local, file.get('Key'))
+            if not os.path.exists(os.path.dirname(dest_pathname)):
+                os.makedirs(os.path.dirname(dest_pathname))
+            if not file.get('Key').endswith('/'):
+                resource.meta.client.download_file(bucket, file.get('Key'), dest_pathname)
 
-BUCKET_NAME = 'mbti-predict-s3' 
-OBJECT_NAME = ['mbti_model.h5'] 
-PATH_NAME = '/tmp/'
-
-def download_s3_folder(bucket_name, s3_folder, local_dir=None):
-    """
-    Download the contents of a folder directory
-    Args:
-        bucket_name: the name of the s3 bucket
-        s3_folder: the folder path in the s3 bucket
-        local_dir: a relative or absolute directory path in the local file system
-    """
-    bucket = s3.Bucket(bucket_name)
-    for obj in bucket.objects.filter(Prefix=s3_folder):
-        target = obj.key if local_dir is None \
-            else os.path.join(local_dir, os.path.relpath(obj.key, s3_folder))
-        if not os.path.exists(os.path.dirname(target)):
-            os.makedirs(os.path.dirname(target))
-        if obj.key[-1] == '/':
-            continue
-        bucket.download_file(obj.key, target)
-
-download_s3_folder(BUCKET_NAME, '/cache','/tmp')
-        
-for obj in OBJECT_NAME:
-  s3.download_file(BUCKET_NAME, obj, PATH_NAME+obj)
-
-os.system("pwd")
+client = boto3.client('s3')
+resource = boto3.resource('s3')
+download_dir(client, resource, 'clientconf/', '/tmp', bucket='mbti-predict-s3')
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
