e23c30a3ab1ad9237d5c06af0d84e8c0361c6afb
emptyinteger <92441821+emptyinteger@users.noreply.github.com>
2022-10-31 01:01:20 +0900

    Update Lambda_function.py

--- Lambda_function.py
+++ Lambda_function.py
@@ -27,10 +27,10 @@ for obj in OBJECT_NAME:
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 SEQ_LEN = 512
-tokenizer = BertTokenizer.from_pretrained('./tmp', local_files_only=True)
+tokenizer = BertTokenizer.from_pretrained('./tmp', local_files_only=True, cache_dir='./tmp')
 
 def create_mbti_bert():
-  model = TFBertModel.from_pretrained('./tmp', local_files_only=True)
+  model = TFBertModel.from_pretrained('./tmp', local_files_only=True, cache_dir='./tmp')
   token_inputs = tf.keras.layers.Input((SEQ_LEN,), dtype=tf.int32, name='input_word_ids')
   mask_inputs = tf.keras.layers.Input((SEQ_LEN,), dtype=tf.int32, name='input_masks')
   segment_inputs = tf.keras.layers.Input((SEQ_LEN,), dtype=tf.int32, name='input_segment')
