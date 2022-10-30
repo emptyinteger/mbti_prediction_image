c938a6884937643d4cbdbc6c7165c9daf69b666b
emptyinteger <92441821+emptyinteger@users.noreply.github.com>
2022-10-31 02:45:25 +0900

    Update Lambda_function.py

--- Lambda_function.py
+++ Lambda_function.py
@@ -19,14 +19,32 @@ import zipfile
 s3 = boto3.client('s3')
 
 BUCKET_NAME = 'mbti-predict-s3' 
-OBJECT_NAME = ['mbti_model.h5','cache.zip'] 
+OBJECT_NAME = ['mbti_model.h5'] 
 PATH_NAME = '/tmp/'
 
+def download_s3_folder(bucket_name, s3_folder, local_dir=None):
+    """
+    Download the contents of a folder directory
+    Args:
+        bucket_name: the name of the s3 bucket
+        s3_folder: the folder path in the s3 bucket
+        local_dir: a relative or absolute directory path in the local file system
+    """
+    bucket = s3.Bucket(bucket_name)
+    for obj in bucket.objects.filter(Prefix=s3_folder):
+        target = obj.key if local_dir is None \
+            else os.path.join(local_dir, os.path.relpath(obj.key, s3_folder))
+        if not os.path.exists(os.path.dirname(target)):
+            os.makedirs(os.path.dirname(target))
+        if obj.key[-1] == '/':
+            continue
+        bucket.download_file(obj.key, target)
 
+download_s3_folder(BUCKET_NAME, '/cache','/tmp')
+        
 for obj in OBJECT_NAME:
   s3.download_file(BUCKET_NAME, obj, PATH_NAME+obj)
 
-zipfile.ZipFile('/tmp/cache.zip').extractall('/tmp')
 os.system("pwd")
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
