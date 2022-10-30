bfad2980345baa59ef5bcbcd0512c39430b84b12
emptyinteger <92441821+emptyinteger@users.noreply.github.com>
2022-10-31 03:56:59 +0900

    Update Lambda_function.py

--- Lambda_function.py
+++ Lambda_function.py
@@ -14,20 +14,23 @@ import re
 import tensorflow_addons as tfa
 import io
 import boto3
-from cloudpathlib import CloudPath
 
-# 버킷명
-BUCKET_NAME = 'mbti-predict-s3'  
-# 객체명
-OBJECT_NAME = 'mbti_model.h5' 
-# 파일을 저장할 위치, AWS Lambda에서는 오직 /tmp 에만 파일을 작성할 수 있다.
-FILE_NAME = '/tmp/mbti_model.h5' 
-
-s3 = boto3.client('s3')
-s3.download_file(BUCKET_NAME, OBJECT_NAME, FILE_NAME)
-
-cp = CloudPath("s3://mbti-predict-s3/cache/")
-cp.download_to("/tmp")
+def download_dir(client, resource, dist, local='/tmp', bucket='your_bucket'):
+    paginator = client.get_paginator('list_objects')
+    for result in paginator.paginate(Bucket=bucket, Delimiter='/', Prefix=dist):
+        if result.get('CommonPrefixes') is not None:
+            for subdir in result.get('CommonPrefixes'):
+                download_dir(client, resource, subdir.get('Prefix'), local, bucket)
+        for file in result.get('Contents', []):
+            dest_pathname = os.path.join(local, file.get('Key'))
+            if not os.path.exists(os.path.dirname(dest_pathname)):
+                os.makedirs(os.path.dirname(dest_pathname))
+            if not file.get('Key').endswith('/'):
+                resource.meta.client.download_file(bucket, file.get('Key'), dest_pathname)
+def _start():
+    client = boto3.client('s3')
+    resource = boto3.resource('s3')
+    download_dir(client, resource, 'clientconf/', '/tmp', bucket='mbti-predict-s3')
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
